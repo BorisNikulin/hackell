69fc8f3fc02acf49443a13b140fee1d0e3e90e39
Boris Nikulin <NikulinBE@gmail.com>
2017-03-01 05:05:50 -0800

    refactor parse function into parseAType, parseCType, and parse

--- Main.hs
+++ Main.hs
@@ -47,18 +47,33 @@ data Op = Add | Sub | And | Or
 
 parse :: T.Text -> Either String Instruction
 parse s = let s' = T.strip s
-          in case T.uncons s' of
-                  Just ('@', a) -> case T.decimal a of
-                                        Right (num, t) -> if T.null t
-                                                            then if isATypeNumInRange num
-                                                                   then Right $ AType $ Left num
-                                                                   else Left "Number in A Type Instruction exceeding 15 bits"
-                                                            else Left "Trailing non digit characters after A Type Numeric Instruction"
-                                        Left _         -> if T.null a
-                                                            then Left "A Type command needs a symbol or a number"
-                                                            else Right $ AType $ Right a --symbol
+          in if isAType s'
+               then parseAType s'
+               else parseCType s'     
+
+--php regex ^(?>(A?M?D?)\s*=)?\s*(?>(0|-?1)|([AMD])\s*([-+&|])\s*([01AMD])|([-!]?[AMD]))(?>\s*;\s*(JMP|JEQ|JNE|JLT|JGE|JGT|JLE))?$
+
+isAType :: T.Text -> Bool
+isAType = startsWith '@'
+    where startsWith c s = case T.uncons s of
+                                Just ('@', _) -> True
+                                _             -> False
+
+parseAType :: T.Text -> Either String Instruction
+parseAType s = case T.decimal $ T.tail s of
+                    Right (num, t) -> if T.null t
+                                        then if isATypeNumInRange num
+                                               then Right $ AType $ Left num
+                                               else Left "Number in A Type Instruction exceeding 15 bits"
+                                        else Left "Trailing non digit characters after A Type Numeric Instruction"
+                    Left _         -> if T.null s
+                                        then Left "A Type command needs a symbol or a number"
+                                        else Right $ AType $ Right s --symbol
+
+--parseCType
 
 
+--A Type numbers can only be 15 bits wide
 isATypeNumInRange num
     | num > 65535 || num < 0 = False
     | otherwise              = True
